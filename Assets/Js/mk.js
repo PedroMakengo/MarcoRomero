dec140f05a9ebc84ee39abd06904e849e1f3cfe4
Pedro Makengo <pedromakengomiguel@gmail.com>
2021-04-19 10:41:42 +0100

    Finalizando a versão Desktop

--- Assets/Js/mk.js
+++ Assets/Js/mk.js
@@ -5,12 +5,12 @@ $(function () {
     }, $(document).load());
 
     // Função de ativo
-    function mudarAtivo(element){
-        $("body").on("click", element, function(){
-            
-            if($(this).hasClass("active")){
+    function mudarAtivo(element) {
+        $("body").on("click", ".navItem", function (e) {
+            e.preventDefault();
+            if ($(this).hasClass("active")) {
                 return;
-            }else {
+            } else {
                 $(element).removeClass("active");
                 $(this).addClass("active");
             }
@@ -18,5 +18,41 @@ $(function () {
     }
     mudarAtivo(".navItem");
 
-    
-}, (jQuery));
+    // Alterar as telas
+    function mudarTelas() {
+        $(".navegacaoHeader li a").map((element, index) => {
+            // console.log(index);
+            $(index).on("click", function (e) {
+                let id = $(this).attr("id");
+                $("section").map((el, index) => {
+                    let classeSessao = $(index).hasClass(id);
+                    if (classeSessao == true) {
+                        let page = $(index).removeClass("dsNone");
+                    } else {
+                        $(index).addClass("dsNone").removeClass("dsActivo");
+                    }
+                });
+            });
+        });
+    }
+    mudarTelas();
+
+    function btnProfissionalEducacao() {
+        $("#btnProfissional").on("click", function () {
+            $("#sessaoEducacao").fadeOut(400, function () {
+                $("#sessaoProfissional").addClass("animated bounceInLeft slower").fadeIn(400);
+            });
+        });
+
+        $("#btnEducacao").on("click", function () {
+            $("#sessaoProfissional").fadeOut(400, function () {
+                $("#sessaoEducacao").addClass("animated bounceInRight").fadeIn(400);
+            });
+        });
+    }
+    btnProfissionalEducacao();
+
+
+
+
+});
